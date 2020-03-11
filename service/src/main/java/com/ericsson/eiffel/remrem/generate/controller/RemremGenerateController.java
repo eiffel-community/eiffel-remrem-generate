d567f29fc56eef557cdaed04abb24c10fc34db30
Sudharshan Bandaru <sudharshan.bandaru@ericsson.com>
2020-03-11 19:41:56 +0530

    Changed the ER lookup parameters names connectToExternalERs to lookupInExternalERs and limit to lookupLimit

--- service/src/main/java/com/ericsson/eiffel/remrem/generate/controller/RemremGenerateController.java
+++ service/src/main/java/com/ericsson/eiffel/remrem/generate/controller/RemremGenerateController.java
@@ -98,12 +98,12 @@ public class RemremGenerateController {
             @ApiParam(value = "message type", required = true) @RequestParam("msgType") final String msgType,
             @ApiParam(value = "ER lookup result multiple found, Generate will fail") @RequestParam(value = "failIfMultipleFound", required = false, defaultValue = "false") final Boolean failIfMultipleFound,
             @ApiParam(value = "ER lookup result none found, Generate will fail") @RequestParam(value = "failIfNoneFound", required = false, defaultValue = "false") final Boolean failIfNoneFound,
-            @ApiParam(value = RemremGenerateServiceConstants.CONNECT_TO_EXTERNAL_ERS) @RequestParam(value = "connectToExternalERs", required = false, defaultValue = "true")  final Boolean connectToExternalERs,
-            @ApiParam(value = RemremGenerateServiceConstants.LIMIT) @RequestParam(value = "limit", required = false, defaultValue = "1") final int limit,
+            @ApiParam(value = RemremGenerateServiceConstants.LOOKUP_IN_EXTERNAL_ERS) @RequestParam(value = "lookupInExternalERs", required = false, defaultValue = "true")  final Boolean lookupInExternalERs,
+            @ApiParam(value = RemremGenerateServiceConstants.LOOKUP_LIMIT) @RequestParam(value = "lookupLimit", required = false, defaultValue = "1") final int lookupLimit,
             @ApiParam(value = "JSON message", required = true) @RequestBody JsonObject bodyJson) {
 
         try {
-            bodyJson = erLookup(bodyJson, failIfMultipleFound, failIfNoneFound, connectToExternalERs, limit);
+            bodyJson = erLookup(bodyJson, failIfMultipleFound, failIfNoneFound, lookupInExternalERs, lookupLimit);
             MsgService msgService = getMessageService(msgProtocol);
             String response;
             if (msgService != null) {
@@ -132,7 +132,7 @@ public class RemremGenerateController {
     }
 
     private JsonObject erLookup(final JsonObject bodyJson, Boolean failIfMultipleFound, Boolean failIfNoneFound,
-    		final Boolean connectToExternalERs, final int limit)
+    		final Boolean lookupInExternalERs, final int lookupLimit)
             throws REMGenerateException {
 
         // Checking ER lookup enabled or not
@@ -145,7 +145,7 @@ public class RemremGenerateController {
 
                     // prepare ER Query
                     String Query = ERLookupController.getQueryfromLookup(lookupLinks.get(i).getAsJsonObject());
-                    String url = erlookupConfig.getErURL() + Query + String.format("&shallow=%s&pageSize=%d",connectToExternalERs, limit);
+                    String url = erlookupConfig.getErURL() + Query + String.format("&shallow=%s&pageSize=%d", lookupInExternalERs, lookupLimit);
 
                     // Execute ER Query
                     int j = 0;
