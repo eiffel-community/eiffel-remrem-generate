ebfbe9afe11b2a4e29484f6a017451ca3e615e18
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2021-09-08 14:57:34 +0200

    Fix spelling error

--- service/src/main/java/com/ericsson/eiffel/remrem/generate/constants/RemremGenerateServiceConstants.java
+++ service/src/main/java/com/ericsson/eiffel/remrem/generate/constants/RemremGenerateServiceConstants.java
@@ -30,9 +30,9 @@ public final class RemremGenerateServiceConstants {
         public static final String DOCUMENTATION_URL = "https://eiffel-community.github.io/eiffel-remrem-generate/index.html";
 
         public static final String UNAVAILABLE_FOR_FAILIFMULTIPLEFOUND = "{\"status_code\": 417, \"result\": \"FAIL\", "
-                + "\"message\":\"Muliple event ids found with ERLookup properties\"}";
+                + "\"message\":\"Multiple event IDs found with ERLookup properties\"}";
 
         public static final String UNAVAILABLE_FOR_FAILIFNONEFOUND = "{\"status_code\": 406, \"result\": \"FAIL\", "
-                + "\"message\":\"No event id found with ERLookup properties\"}";
+                + "\"message\":\"No event ID found with ERLookup properties\"}";
 
 }
