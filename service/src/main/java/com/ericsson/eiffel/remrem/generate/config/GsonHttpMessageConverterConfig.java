30b01b17632319a5be7a18735428b6134d5386ea
tobiastechnic <tobias.akervik@ericsson.com>
2026-03-24 13:40:27 +0100

    Uplift OpenAPi and Swagger-UI to latest version. (#248)
    * Uplift OpenAPi and Swagger-UI to latest version.
    
    * minor coment fix
    
    * Some Review comments updates
    
    * change swagger-ui version to a property in pom file
    
    * Fix application version in Swagger-ui which only works for execution from production war artifact.
    
    * Updated changelog.md
    
    * remove resource filtering in service/pom.xml file which is not needed

--- service/src/main/java/com/ericsson/eiffel/remrem/generate/config/GsonHttpMessageConverterConfig.java
+++ service/src/main/java/com/ericsson/eiffel/remrem/generate/config/GsonHttpMessageConverterConfig.java
@@ -1,17 +1,3 @@
-/*
-    Copyright 2018 Ericsson AB.
-    For a full list of individual contributors, please see the commit history.
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
 package com.ericsson.eiffel.remrem.generate.config;
 
 import com.google.gson.Gson;
@@ -19,7 +5,6 @@ import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import springfox.documentation.spring.web.json.Json;
 
 @Configuration
 public class GsonHttpMessageConverterConfig {
@@ -32,8 +17,6 @@ public class GsonHttpMessageConverterConfig {
     }
 
     private Gson gson() {
-        final GsonBuilder builder = new GsonBuilder();
-        builder.registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter());
-        return builder.create();
+        return new GsonBuilder().create();
     }
 }
